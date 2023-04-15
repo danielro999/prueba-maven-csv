062e14c05a43aa42fee1a230c12193065a9adfaa
danielro999 <persensus@yahoo.com.ar>
2023-04-15 18:11:47 -0300

    conexion a base de datos local ok!

--- src/main/java/pruebaMaven/Main.java
+++ src/main/java/pruebaMaven/Main.java
@@ -6,6 +6,10 @@ import modelo.LectorArchivo;
 import modelo.Partido;
 import modelo.Pronostico;
 
+import java.sql.*;
+
+import static coneccion.ConectorSQL.*;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -60,9 +64,61 @@ public class Main {
 		
 		System.out.println("puntos por pronostico argentina - arabia = " + pronostico1.puntos());
 		System.out.println("puntos por pronostico mexico - polonia = " + pronostico2.puntos());
-	
-		System.out.println(pronostico1.getResultado());
-		System.out.println(pronostico2.getResultado());
-	}
 
+		Connection conexion = null;
+		Statement consulta = null;
+
+		try {
+
+			// Abrir la conexión
+			System.out.println("conectando a la base de datos...");
+
+			conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Ejecutar una consulta
+			System.out.println("Creating statement...");
+			consulta = conexion.createStatement();
+			String sql;
+			sql = "SELECT nombre FROM prode.equipo";
+
+			//En la variable resultado obtendremos las distintas filas que nos devolvió la base
+			ResultSet resultado = consulta.executeQuery(sql);
+
+			// Obtener las distintas filas de la consulta
+			while (resultado.next()) {
+				// Pbtener el valor de cada columna
+
+				String Nombre = resultado.getString("nombre");
+
+
+				// Mostrar los valores obtenidos
+
+				System.out.println("nombre: " + Nombre);
+
+			}
+			// Esto se utiliza par cerrar la conexión con la base de datos
+			resultado.close();
+			consulta.close();
+			conexion.close();
+		} catch (SQLException se) {
+			// Execpción ante problemas de conexión
+			se.printStackTrace();
+		} finally {
+			// Esta sentencia es para que ante un problema con la base igual se cierren las conexiones
+			try {
+				if (consulta != null)
+					consulta.close();
+			} catch (SQLException se2) {
+			}
+			try {
+				if (conexion != null)
+					conexion.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		System.out.println("Fin de la ejecucción");
+	}
 }
+
+
