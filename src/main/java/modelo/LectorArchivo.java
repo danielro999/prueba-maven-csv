39e8bdf816d20208e6363e284eeee54aca63bcfc
daniel <persensus@yahoo.com.ar>
2023-04-26 19:24:47 -0300

    cambio en el archivo csv ; => , y en LectorArchivo a split(,)

--- src/main/java/modelo/LectorArchivo.java
+++ src/main/java/modelo/LectorArchivo.java
@@ -30,7 +30,7 @@ public class LectorArchivo{
                     // con esta configuración podemos skipear la primera línea de nuestro archivo CSV
                     .withSkipLines(1)
                     // con esta configuración podemos elegir cual es el caracter que vamos a usar para delimitar
-                    .withSeparator(';')
+                    .withSeparator(',')
                     // Es necesario definir el tipo de dato que va a generar el objeto que estamos queriendo parsear a partir del CSV
                     .withType(DatosResultado.class)
                     .build()
